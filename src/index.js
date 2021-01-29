2987a85b47ff8b79b55aac0952fa285b7b7290b0
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-28 20:24:30 -0800

    adding the spread operator

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//spread operator
+// const cirtrus = ["orange", "lime", "lemon"];
+// const fruits = ["Apple", ...cirtrus];
+
+// const person = {
+//   fName: "bone",
+//   lName: "Grey"
+// };
+
+// const user = {
+//   ...person,
+//   userId: 1
+// };
+
+// console.log(user);
